22c45af1882672f3485387a45a91f6ad9660d0ec
Trung DG <thanhtrung.it25@gmail.com>
2021-04-15 16:40:40 +0700

    Post: Controller, routing, and module structure

---
+++ src/posts/dto/updatePost.dto.ts
@@ -0,0 +1,7 @@
+class UpdatePostDto {
+  id: number;
+  title: string;
+  content: string;
+}
+
+export default UpdatePostDto;
---
+++ src/posts/posts.controller.ts
@@ -0,0 +1,43 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import CreatePostDto from './dto/createPost.dto';
+import UpdatePostDto from './dto/updatePost.dto';
+import PostsService from './posts.service';
+
+@Controller('posts')
+export default class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
+  @Get()
+  getAllPosts() {
+    return this.postsService.getAllPosts();
+  }
+
+  @Get(':id')
+  getPostById(@Param('id') id: string) {
+    return this.postsService.getPostById(Number(id));
+  }
+
+  @Post()
+  createPost(@Body() post: CreatePostDto) {
+    console.log(post);
+    return this.postsService.createPost(post);
+  }
+
+  @Put(':id')
+  replacePost(@Param('id') id: string, @Body() post: UpdatePostDto) {
+    return this.postsService.replacePost(Number(id), post);
+  }
+
+  @Delete(':id')
+  deletePost(@Param('id') id: string) {
+    return this.postsService.deletePost(Number(id));
+  }
+}
